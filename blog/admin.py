2ee534d83c3b6bcfb2a728c2bd617676ac7e67df
Umam Ahmad <umamahmad2006@gmail.com>
2026-03-17 16:26:31 +0530

    added the comment to admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 
@@ -16,3 +16,9 @@ class PostAdmin(admin.ModelAdmin):
     ordering = ['status', 'publish']
     autocomplete_fields = ['author']
     ordering = ['-publish']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
