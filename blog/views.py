99d30df018272249a1820574d3363c2b4feab444
Umam Ahmad <umamahmad2006@gmail.com>
2026-03-18 14:33:33 +0530

    .

--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,9 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from .forms import EmailPostForm, CommentForm
 
 
 
