5818cccf0f4f02e5c33a1789a97226a48a68dbfd
Umam Ahmad <umamahmad2006@gmail.com>
2026-03-28 22:40:00 +0530

    added similar posts button

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .models import Post
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
@@ -138,13 +139,23 @@ def post_detail(request, year, month, day, post):
     # Form for users to comment
     form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.objects.filter(
+        tags__in=post_tags_ids
+        ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+        ).order_by('-same_tags', '-publish')[:4]
+
     return render(
         request,
         'blog/post/detail.html',
         {
             'post': post,
             'comments': comments,
-            'form': form 
+            'form': form,
+            'similar_posts': similar_posts 
         }
     )
     
