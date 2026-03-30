f19b126b75501bcb9ed960689813b1d2dc23b29e
Umam Ahmad <umamahmad2006@gmail.com>
2026-03-30 13:27:38 +0530

    updated blog/url

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 urlpatterns = [
@@ -17,5 +18,6 @@ urlpatterns = [
 
     path(
         'tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'
-    )
+    ),
+    path('feed/', LatestPostsFeed(), name='post_feed')
 ]
