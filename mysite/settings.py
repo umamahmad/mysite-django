c3f61c4dd66d7ceaa70e03fdb35d515a5a0a62c3
Umam Ahmad <umamahmad2006@gmail.com>
2026-04-06 16:23:14 +0530

    added 'django.contrib.postgres', to settings for lookups

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.sitemaps',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'taggit',
     'blog.apps.BlogConfig',
 ]
